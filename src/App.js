a6550ffd0b5818cb7914a1048314649aef5269a9
Annie <colberta31@gmail.com>
2024-01-09 11:18:45 -0500

    added bootstrap
    added bootstrap and added legacy files to Json

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import "./App.css";
+import React from "react";
 
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather Application</h1>
+      <div className="container">
+        <h1> Weather Application</h1>
 
-      <footer>
-        {" "}
-        This project is open-sourced by Annie Colbert and is{" "}
-        <a href="open-sourced" target="blank">
-          open-sourced
-        </a>
-      </footer>
+        <footer>
+          {" "}
+          This project is open-sourced by Annie Colbert and is{" "}
+          <a href="open-sourced" target="blank">
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
