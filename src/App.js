f6420e68d85bd7446aa0fee6282441370431dc86
Annie <colberta31@gmail.com>
2024-01-09 10:45:05 -0500

    Added GitHub link
    added Github link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ export default function App() {
   return (
     <div className="App">
       <h1> Weather Application</h1>
+
+      <footer>
+        {" "}
+        This project is open-sourced by Annie Colbert and is{" "}
+        <a href="open-sourced" target="blank">
+          open-sourced
+        </a>
+      </footer>
     </div>
   );
 }
